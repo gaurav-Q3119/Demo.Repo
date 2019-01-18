50b84d48fb04a6a50e30a1c308ff00f0720bd099
gaurav-Q3119 <gaurav.n.s.3@gmail.com>
2019-01-18 07:26:33 +0530

    This is new commit for 18 Jan

---
+++ src/test/java/com/qa/tests/HomePageTest.java
@@ -0,0 +1,13 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+	@Test
+	public void hpageTest(){
+		
+		System.out.println("this is homepage Test");
+	}
+	
+}
